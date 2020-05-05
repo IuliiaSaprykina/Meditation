487d137b34aa11fa2fd73b477c0f85a1ae5f32c0
wolf-dominion <hannah_drury@yahoo.com>
2020-05-04 20:02:14 -0600

    Fixed book list new line issue

--- frontend/userShowPage.js
+++ frontend/userShowPage.js
@@ -9,41 +9,40 @@ console.log(bookNewLineSelect)
 
 fetch(usersURL)
     .then(parseJSON)
-    //.then(user => console.log(user))
     .then(displayUser)
-
-//     function displayUsers(users){
-//         users.forEach(user => {
-//             displayUsersOptions(user)
-//         });
-//         return users
-//     }
-
-function displayUser(user){
-    const h2 = document.createElement('h2');
-    const h2_2 = document.createElement('h2');
-    const h3 = document.createElement('a');
-    
-    let booksList = user.books;
-    
-    
-    h2.innerText = user.name;
-    h2_2.innerText = "Books to read: "
-    booksList.forEach (book => {
-        let link = document.createTextNode(book.title);
-        h3.appendChild(link)
-        h3.title = book.title
-        h3.href = book.link_url
-        h3.target = "_blank"
-        
-        
-        bookNewLineSelect.append(h3)
-        
-    })
-    userDataSelect.append(h2, h2_2)
-}
 
 function parseJSON(response){
     return response.json()
 }
 
+function displayUser(user){
+    const h1 = document.createElement('h1');
+    h1.innerText = `${user.name}'s Page`;
+    userDataSelect.append(h1)
+    displayBookList(user)
+
+}
+
+function displayBookList(user){
+    let booksList = user.books;
+    const booklist_h3 = document.createElement('h3');
+
+    booklist_h3.innerText = "Books to read: "
+    booksList.forEach (book => {
+        // I moved display booklist into its own function since that is 
+        //different from displaying user info
+        // The reason links were not on a new line: 
+        // h3 was declared outside of the loop, so each book kept being overwritten by the next one 
+        let h3 = document.createElement('a');
+        let link = document.createTextNode(`- ${book.title}`);
+        let linebreak = document.createElement('br');
+
+        h3.appendChild(link)
+        h3.title = book.title
+        h3.href = book.link_url
+        h3.target = "_blank"
+        
+        userDataSelect.append(booklist_h3)
+        bookNewLineSelect.append(h3, linebreak)
+    })
+}
