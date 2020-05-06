4d23d9dcc5373ab67194e114795c05c0886462ed
wolf-dominion <hannah_drury@yahoo.com>
2020-05-06 10:03:15 -0600

    Tried to reset database

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,30 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_212459) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.text "description"
-    t.text "link_url"
-  end
-
-  create_table "user_books", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "book_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_user_books_on_book_id"
-    t.index ["user_id"], name: "index_user_books_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "user_books", "books"
-  add_foreign_key "user_books", "users"
 end
